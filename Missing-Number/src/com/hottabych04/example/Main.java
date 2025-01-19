656b96eb530da511894909dfc70767dd9fb8f1bc
hottabych04 <hottabych04@gmail.com>
2025-01-19 15:45:33 +0300

    Missing Number

---
+++ Missing-Number/src/com/hottabych04/example/Main.java
@@ -0,0 +1,26 @@
+package com.hottabych04.example;
+
+import java.util.Scanner;
+
+public class Main {
+    private final static Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        int[] nums = parseStringToIntArray(line);
+
+        System.out.println(solution.missingNumber(nums));
+    }
+
+    public static int[] parseStringToIntArray(String input) {
+        String[] stringNumbers = input.replaceAll("\\[|\\]|\\s", "").split(",");
+
+        int[] numbers = new int[stringNumbers.length];
+        for (int i = 0; i < stringNumbers.length; i++) {
+            numbers[i] = Integer.parseInt(stringNumbers[i]);
+        }
+        return numbers;
+    }
+}
